09b15e9b366ac120c0131ee6e0d9ae894d404242
Andrew Thomas <ajthomas145@gmail.com>
2021-10-16 16:01:18 +0100

    1st refector

--- js/app.js
+++ js/app.js
@@ -3,33 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const newStarFormSubmit = function(event) {
         event.preventDefault();
 
-        const name = event.target.name.value;
-        const sport = event.target.sport.value;
-        const nationality = event.target.nationality.value;
-
-        const newStarList = document.createElement("li");
-        
-        const nameElement = document.createElement("h1");
-        nameElement.textContent = name;
-        newStarList.appendChild(nameElement);
-
-        const sportElement = document.createElement("h2");
-        sportElement.textContent = sport;
-        newStarList.appendChild(sportElement);
-
-        const nationalityElement = document.createElement("h3");
-        nationalityElement.textContent = nationality;
-        newStarList.appendChild(nationalityElement);
-
-        const sportStarLIst = document.querySelector("#sports-star-list");
-        sportStarLIst.appendChild(newStarList);
+        const sportStarList = createSportStar(event);
+        const sportStar = document.querySelector("#sports-star-list");
+        sportStar.appendChild(sportStarList);
 
         event.target.reset();
 
-
     }
-
-
+    
     const newStarForm = document.querySelector("#new-sports-star");
     newStarForm.addEventListener("submit", newStarFormSubmit);
 
@@ -41,11 +22,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteAllButton = document.querySelector("#delete-all");
     deleteAllButton.addEventListener("click", deleteAllClick)
 
-
- 
-
-
   })
+
+
+
+
+  const createSportStar = function(event) {
+
+    const name = event.target.name.value;
+    const sport = event.target.sport.value;
+    const nationality = event.target.nationality.value;
+
+    const newStarList = document.createElement("li");
+        
+    const nameElement = document.createElement("h1");
+    nameElement.textContent = name;
+    newStarList.appendChild(nameElement);
+
+    const sportElement = document.createElement("h2");
+    sportElement.textContent = sport;
+    newStarList.appendChild(sportElement);
+
+    const nationalityElement = document.createElement("h3");
+    nationalityElement.textContent = nationality;
+    newStarList.appendChild(nationalityElement);
+
+    return newStarList;
+
+}
 
 // 1. once data is entered, grab that data - done
 // 2.add that data to an element - done
